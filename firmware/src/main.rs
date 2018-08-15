77adea51beeeb682ff56916ffa2659061123f12a
Danilo Bargen <mail@dbrgn.ch>
2018-08-15 23:57:39 +0200

    firmware: Set up serial communication

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,12 +3,14 @@
 
 extern crate cortex_m;
 #[macro_use(entry, exception)] extern crate cortex_m_rt as rt;
+#[macro_use(block)] extern crate nb;
 extern crate panic_semihosting;
 extern crate stm32f103xx;
 extern crate stm32f103xx_hal as hal;
 
 use hal::delay::Delay;
 use hal::prelude::*;
+use hal::serial::Serial;
 use rt::ExceptionFrame;
 
 // Entry point
@@ -18,8 +20,10 @@ fn main() -> ! {
     let dp = stm32f103xx::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
 
-    // Get reference to GPIO peripherals
     let mut rcc = dp.RCC.constrain();
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    // Get reference to GPIO peripherals
     let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
 
@@ -33,10 +37,30 @@ fn main() -> ! {
     let _pin_down = gpiob.pb5.into_push_pull_output(&mut gpiob.crl);
     let mut pin_led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
+    // Set up serial communication
+    let tx_pin = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+    let rx_pin = gpiob.pb7;
+    let serial = Serial::usart1(
+        dp.USART1,
+        (tx_pin, rx_pin),
+        &mut afio.mapr,
+        9600.bps(),
+        clocks,
+        &mut rcc.apb2,
+    );
+    let (mut tx, _rx) = serial.split();
+
     // Main loop
     loop {
+        block!(tx.write(b'H')).ok();
+        block!(tx.write(b'i')).ok();
+        block!(tx.write(b'\n')).ok();
         pin_led.set_high();
         delay.delay_ms(1_000_u16);
+
+        block!(tx.write(b'L')).ok();
+        block!(tx.write(b'o')).ok();
+        block!(tx.write(b'\n')).ok();
         pin_led.set_low();
         delay.delay_ms(1_000_u16);
     }
